704e3d50f13c5252fe597e6d293984e7c0a527e4
Ben Mills <millsb@reanstudios.com>
2009-10-05 20:54:21 -0600

    Added capistrano for deployment

---
+++ config/deploy.rb
@@ -0,0 +1,49 @@
+set :application, "prb"
+set :repository,  "git@reanstudios.com:prb"
+set :deploy_to, "/Library/rails/#{application}"
+
+set :scm, :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :branch, 'master'
+set :git_shallow_clone, 1
+set :scm_verbose, true
+set :use_sudo, false
+set :ssh_options, {:forward_agent => true}
+on :start do    
+  `ssh-add`
+end
+
+role :web, "reanstudios.com"                          # Your HTTP server, Apache/etc
+role :app, "reanstudios.com"                          # This may be the same as your `Web` server
+role :db,  "reanstudios.com", :primary => true # This is where Rails migrations will run
+
+# If you are using Passenger mod_rails uncomment this:
+# if you're still using the script/reapear helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# namespace :deploy do
+#   task :start {}
+#   task :stop {}
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
+
+####################################
+## NOTHING BELOW SHOULD BE EDITED ##
+####################################
+ 
+####
+## Custom Deploy Namespace for deployment on Thin
+namespace :deploy do  
+  
+  desc 'Restart Passenger processes on the app servers.'
+  task :restart, :roles => :app do
+    #run "pkill dispatch.fcgi"
+  end
+  
+end
+
+after 'deploy:symlink', :roles => :app do
+  run "ln -nfs #{deploy_to}/#{shared_dir}/config/database.yml #{current_path}/config/database.yml"
+end
