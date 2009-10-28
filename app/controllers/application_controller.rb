af719d67ee6077c5cd09d3afbacfcd6552824fbf
Ben Mills <millsb@reanstudios.com>
2009-10-28 13:52:52 -0600

    Changed url_status checker to validate against array of acceptable HTTP responses.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,14 @@ class ApplicationController < ActionController::Base
   # filter_parameter_logging :password
   
   def url_lookup(url)
+    @valid_responses = ["200", "301"]
+    
 	  @curl = %x{ curl -s --connect-timeout 3 -I #{url} --user-agent "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_1; en-us) AppleWebKit/531.9 (KHTML, like Gecko) Version/4.0.3 Safari/531.9" | grep HTTP }
-    return @curl[/\d\d\d/]
+	  
+    if @valid_responses.include?(@curl[/\d\d\d/])
+      return true
+    else
+      return false
+    end
 	end	
 end
