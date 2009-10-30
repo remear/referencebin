0a2421835b14b66f01ca7ade799666fa56c97e64
Ben Mills <millsb@reanstudios.com>
2009-10-29 19:08:15 -0600

    Fixed permalink generator

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -3,8 +3,8 @@ class Bookmark < ActiveRecord::Base
   belongs_to :user
   has_many :comments
   has_many :questions
-  before_save :generate_permalink
-  after_save :generate_thumb
+  after_create :generate_permalink
+  after_create :generate_thumb
   
   acts_as_taggable
   
@@ -22,20 +22,10 @@ class Bookmark < ActiveRecord::Base
       
   private
     def generate_permalink
-      title_based_permalink = self.title.gsub(" ", "-")
-      title_based_permalink = title_based_permalink.match('([A-Za-z0-9\-])+').to_s
-      @last_record = Bookmark.find(:last, :select => "id")
-      if @last_record.nil?
-        id = 1
-      else
-        id = @last_record.id + 1
-      end
-      self.permalink = "#{id}-#{title_based_permalink.downcase}"
+      update_attributes(:permalink => "#{self.id}-#{self.title.parameterize}") 
     end
   
     def generate_thumb
-      @bookmark = Bookmark.find(:last, :select => "id")
-      
-      Delayed::Job.enqueue(DelayedThumbnail.new(@bookmark.id), 0, 10.minutes.from_now)
+      Delayed::Job.enqueue(DelayedThumbnail.new(self.id), 0, 10.minutes.from_now)
     end
 end
