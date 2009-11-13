d93f7f81e03719bc5c89d69eadf738692a7b924a
Ben Mills <millsb@reanstudios.com>
2009-11-13 00:26:41 -0700

    General cleanup

--- db/migrate/20090923170024_add_language_id_to_code.rb
+++
@@ -1,9 +0,0 @@
-class AddLanguageIdToCode < ActiveRecord::Migration
-  def self.up
-    add_column :codes, :language_id, :integer
-  end
-
-  def self.down
-    remove_column :codes, :language_id
-  end
-end
