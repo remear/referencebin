6d2e6e24370ece11c8900060e64b60a9acc334c9
Ben Mills <millsb@reanstudios.com>
2009-11-12 23:48:46 -0700

    Removed questions controller

--- app/models/question.rb
+++
@@ -1,8 +0,0 @@
-class Question < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :bookmark
-  belongs_to :language
-  belongs_to :code
-
-  attr_accessible :body, :user_id, :bookmark_id, :count
-end
--- test/unit/question_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class QuestionTest < ActiveSupport::TestCase
-  def test_should_be_valid
-    assert Question.new.valid?
-  end
-end
