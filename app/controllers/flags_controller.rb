08b877d1331452de5b3c4dc6309feae7ff046122
Ben Mills <millsb@reanstudios.com>
2009-12-10 00:17:55 -0700

    Flags controller create should respond to format.js

--- app/controllers/flags_controller.rb
+++ app/controllers/flags_controller.rb
@@ -6,12 +6,16 @@ class FlagsController < ApplicationController
   def create
     @flag = Flag.new(params[:flag])
     @flag.flaggable_id = params[:id]
+    @flag.flaggable_type = params[:type]
+    
     respond_to do |format|
       if @flag.save
         flash[:notice] = 'Bookmark was successfully created.'
         format.html { redirect_to :back }
+        format.js { render :text => 'true' }
       else
         format.html { render :action => "new" }
+        format.js { render :text => 'false' }
       end
     end
   end
