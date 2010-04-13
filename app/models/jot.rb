ff6eea695e49488c8a0da566f6862978a2b91836
Ben Mills <millsb@reanstudios.com>
2010-04-13 15:32:41 -0600

    Removed syntaxify from jots

--- app/models/jot.rb
+++ app/models/jot.rb
@@ -5,7 +5,7 @@ class Jot < ActiveRecord::Base
   validates_presence_of :language_id
   validates_presence_of :code
   
-  syntaxify :body, :line_numbers => :table, :css => :class
+  #syntaxify :body, :line_numbers => :table, :css => :class
 end
 
 # == Schema Information
